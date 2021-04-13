ef7a1b36db683796757154ccd0f51c5e13a9a44f
theboydontplaygames <52797054+theboydontplaygames@users.noreply.github.com>
2021-04-13 19:13:32 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace OxfordDictionary_RNTTHA006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSubmitRequest = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.txtMatch = new System.Windows.Forms.TextBox();
+            this.txtAPI = new System.Windows.Forms.TextBox();
+            this.txtFields = new System.Windows.Forms.TextBox();
+            this.txtAppKey = new System.Windows.Forms.TextBox();
+            this.txtAppID = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(863, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Word: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "App_ID: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "App_Key: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Fields: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Strict_Match; ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "API_Request: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(348, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Response_Code: ";
+            // 
+            // btnSubmitRequest
+            // 
+            this.btnSubmitRequest.Location = new System.Drawing.Point(446, 81);
+            this.btnSubmitRequest.Name = "btnSubmitRequest";
+            this.btnSubmitRequest.Size = new System.Drawing.Size(146, 53);
+            this.btnSubmitRequest.TabIndex = 8;
+            this.btnSubmitRequest.Text = "Submit_Request";
+            this.btnSubmitRequest.UseVisualStyleBackColor = true;
+            this.btnSubmitRequest.Click += new System.EventHandler(this.btnSubmitRequest_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(100, 46);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(195, 20);
+            this.txtWord.TabIndex = 9;
+            // 
+            // txtMatch
+            // 
+            this.txtMatch.Location = new System.Drawing.Point(100, 208);
+            this.txtMatch.Name = "txtMatch";
+            this.txtMatch.Size = new System.Drawing.Size(181, 20);
+            this.txtMatch.TabIndex = 11;
+            // 
+            // txtAPI
+            // 
+            this.txtAPI.Location = new System.Drawing.Point(100, 255);
+            this.txtAPI.Name = "txtAPI";
+            this.txtAPI.Size = new System.Drawing.Size(619, 20);
+            this.txtAPI.TabIndex = 12;
+            // 
+            // txtFields
+            // 
+            this.txtFields.Location = new System.Drawing.Point(100, 160);
+            this.txtFields.Name = "txtFields";
+            this.txtFields.Size = new System.Drawing.Size(181, 20);
+            this.txtFields.TabIndex = 13;
+            // 
+            // txtAppKey
+            // 
+            this.txtAppKey.Location = new System.Drawing.Point(100, 121);
+            this.txtAppKey.Name = "txtAppKey";
+            this.txtAppKey.Size = new System.Drawing.Size(181, 20);
+            this.txtAppKey.TabIndex = 14;
+            // 
+            // txtAppID
+            // 
+            this.txtAppID.Location = new System.Drawing.Point(100, 81);
+            this.txtAppID.Name = "txtAppID";
+            this.txtAppID.Size = new System.Drawing.Size(252, 20);
+            this.txtAppID.TabIndex = 15;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(446, 174);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(210, 20);
+            this.txtCode.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "API_Response: ";
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(100, 309);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(763, 20);
+            this.txtResponse.TabIndex = 18;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 456);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtAppID);
+            this.Controls.Add(this.txtAppKey);
+            this.Controls.Add(this.txtFields);
+            this.Controls.Add(this.txtAPI);
+            this.Controls.Add(this.txtMatch);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.btnSubmitRequest);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Oxford Dictionary App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSubmitRequest;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.TextBox txtMatch;
+        private System.Windows.Forms.TextBox txtAPI;
+        private System.Windows.Forms.TextBox txtFields;
+        private System.Windows.Forms.TextBox txtAppKey;
+        private System.Windows.Forms.TextBox txtAppID;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace OxfordDictionary_RNTTHA006
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void btnSubmitRequest_Click(object sender, EventArgs e)
+        {
+            //Create new HttpClient. 
+            var client = new HttpClient();
+
+            //Set the HTTP base address.
+            client.BaseAddress = new Uri("https://od-api.oxforddictionaries.com/api/v2/");
+
+            //Set the headers that include the type of request and your app_id and app-key
+            client.DefaultRequestHeaders.Accept.Clear();
+
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("app_id", txtAppID.Text);
+            client.DefaultRequestHeaders.Add("app_key", txtAppKey.Text);
+
+
+            //create the request string from the parameters in your form
+            var requestString = "entries/en-gb/" + txtWord.Text + "?fields=" + txtFields.Text + "&strictMatch=" + txtMatch.Text;
+
+            //display your URL request for debugging purposes
+            txtAPI.Text = client.BaseAddress.ToString() + requestString;
+
+            //Send the HTTP GET request Asynchronously
+            HttpResponseMessage response = await client.GetAsync(requestString);
+
+            //Wait for the server response and read it into the result string
+            string result = await response.Content.ReadAsStringAsync();
+
+            //Display the server response code and response
+            txtCode.Text = response.StatusCode.ToString();
+            txtResponse.Text = result;
+
+
+
+
+
+
+
+
+        }
+    }
+}
